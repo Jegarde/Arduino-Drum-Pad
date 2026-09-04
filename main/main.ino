a6ba04aaf1dffb09f8e8a9c6c895575adb1a39aa
Jesse <jesse.tapani.nieminen@gmail.com>
2026-09-04 20:10:05 +0300

    Implement overwriting note with another

--- main/main.ino
+++ main/main.ino
@@ -10,7 +10,7 @@ int lastButtonStates[BUTTON_COUNT] = {};
 int notes[BUTTON_COUNT] = {NOTE_B5, NOTE_C6, NOTE_D6, NOTE_E6, NOTE_F6, NOTE_G6, NOTE_A6, NOTE_B6};
 
 unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 50;
+unsigned long debounceDelay = 25;
 
 void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
@@ -30,6 +30,8 @@ void initializeButtons() {
 }
 
 void loop() {
+  bool anyButtonsPressed = false;
+
   for (int i = 0; i < BUTTON_COUNT; i++) {
     int buttonPin = buttonPins[i];
     int reading = digitalRead(buttonPin);
@@ -43,16 +45,19 @@ void loop() {
         buttonStates[i] = reading;
 
         if (reading == LOW) {
-          tone(BUZZER_PIN, notes[i], 100);
-          Serial.print(i);
-          Serial.println(" is pressed.");
-        } else {
-          Serial.print(i);
-          Serial.println(" is released.");
+          tone(BUZZER_PIN, notes[i]);
         }
       }
     }
 
     lastButtonStates[i] = reading;
+
+    if (anyButtonsPressed == false) {
+      anyButtonsPressed = buttonStates[i] == LOW;
+    }
+  }
+
+  if (anyButtonsPressed == false) {
+    noTone(BUZZER_PIN);
   }
 }
