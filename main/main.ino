9343431735954979c89c27afd4f221d52b95aea5
Jesse <jesse.tapani.nieminen@gmail.com>
2026-09-04 18:47:12 +0300

    Map the buttons

---
+++ main/main.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // Upper row
+  pinMode(2, INPUT_PULLUP);
+  pinmode(4, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinmode(8, INPUT_PULLUP);
+
+  // Lower row
+  pinMode(3, INPUT_PULLUP);
+  pinmode(5, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinmode(9, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
