0767da55317f544fc72d86adf6eea967b20cb53f
Jesse <jesse.tapani.nieminen@gmail.com>
2026-09-04 19:44:13 +0300

    Implement button input with debounce

--- main/main.ino
+++ main/main.ino
@@ -1,18 +1,50 @@
-void setup() {
-  // Upper row
-  pinMode(2, INPUT_PULLUP);
-  pinmode(4, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
-  pinmode(8, INPUT_PULLUP);
+const int buttonCount = 8;
+int buttonPins[buttonCount] = {2, 4, 6, 8, 3, 5, 7, 9};
+int buttonStates[buttonCount] = {};
+int lastButtonStates[buttonCount] = {};
 
-  // Lower row
-  pinMode(3, INPUT_PULLUP);
-  pinmode(5, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
-  pinmode(9, INPUT_PULLUP);
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+void setup() {
+  initializeButtons();
+  Serial.begin(9600);
+}
+
+void initializeButtons() {
+  for (int i = 0; i < buttonCount; i++) {
+    int buttonPin = buttonPins[i];
+    pinMode(buttonPin, INPUT_PULLUP);
+
+    // Prevent first time false positives
+    buttonStates[i] = digitalRead(buttonPin);
+    lastButtonStates[i] = buttonStates[i];
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (int i = 0; i < buttonCount; i++) {
+    int buttonPin = buttonPins[i];
+    int reading = digitalRead(buttonPin);
 
+    if (reading != lastButtonStates[i]) {
+      lastDebounceTime = millis();
+    }
+
+    if ((millis() - lastDebounceTime) > debounceDelay) {
+      if (reading != buttonStates[i]) {
+        buttonStates[i] = reading;
+
+        if (reading == LOW) {
+          Serial.print(buttonPin);
+          Serial.println(" is pressed.");
+        } else {
+          Serial.print(buttonPin);
+          Serial.println(" is released.");
+        }
+      }
+    }
+
+    lastButtonStates[i] = reading;
+  }
 }
